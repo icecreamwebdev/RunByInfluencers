c42bf59f8a57f747e306c9bf7790f10916873686
icecreamwebdev <icecreamwebdev@gmail.com>
2021-03-13 15:53:19 +0000

    php file

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
         <title>Run By Influencers</title>
-        <meta name="description" content="An agency ran by influencers">
+        <meta name="description" content="RBI is the fastest growing influencer agency, run by actual influencers. Creating clickable global social campaigns, using our unique in house formulas to ensure the best ROI for brands.">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 
@@ -18,85 +18,112 @@
     
     <body>
         
+        <script>document.onreadystatechange = function () {
+  var state = document.readyState
+  if (state == 'complete') {
+      setTimeout(function(){
+          document.getElementById('interactive');
+         document.getElementById('load').style.visibility="hidden";
+      },1000);
+  }
+}</script>
+        
+        <div id="load">
+            <div id="headerLogo"><a href="#"><img src="logonewnew.png" id="headerLogo"></a></div>
+            
+            <img id="preloadIMg" src="preload.jpg" />
+        
+        </div>
+        
+        
         <div id="homeWrap">
+            
+            <nav role="navigation">
+  <div id="menuToggle">
+    <!--
+    A fake / hidden checkbox is used as click reciever,
+    so you can use the :checked selector on it.
+    -->
+    <input type="checkbox" />
+    
+    <!--
+    Some spans to act as a hamburger.
+    
+    -->
+    <span></span>
+    <span></span>
+    <span></span>
+    
+    <!--
+    -->
+    <ul id="menu">
+      <a href="#brandsec"><li>Brands</li></a>
+     <a href="#talentsec" ><li id="middle">Talent</li></a>
+    <a href="#contactsec"><li>Contact</li></a>
+    </ul>
+  </div>
+</nav>
+            
             <header>
                 
-                <div id="headerLogo"><a href="#"><img src="newlogo.jpg" id="headerLogo"></a></div>
+                <div id="headerLogo"><a href="#"><img src="logonewnew.png" id="headerLogo"></a></div>
                 <div id="headerSocials">
                     
+                    
                     <ul>
-                        <a href="#"><li><i class="fab fa-twitter fa-lg"></i></li></a>
-                        <a href="#"><li><i class="fab fa-instagram fa-lg"></i></li></a>
+                        <a href="#brandsec"><li>Brands</li></a>
+                        <a href="#talentsec" ><li id="middle">Talent</li></a>
+                        <a href="#contactsec"><li>Contact</li></a>
                     </ul>
                 
                 </div>
+
             </header>
             
             
             <div id="homeImage">
                 
-                <video autoplay muted loop>
+                <video autoplay playsinline muted loop>
   <source src="homebackground.mp4" type="video/mp4">
                    
 </video>
                 
                 <img src="safetyBackground.jpg" id="safetyBackground" />
                 
-                <div id="homeText">
-                    <h2>DESIGNED</h2>
-                    <h3>DIFFERENTLY</h3>
+               <div id="homeText">
+                    <h2>LET'S CREATE</h2>
+                   <h3>TOGETHER<span>.</span></h3>
                 </div>
                 
-                <a id="downArrow" onclick="scrollWin()"><i class="fas fa-arrow-alt-circle-down fa-2x"></i></a>
+                <a id="downArrow" href="#sec"><i class="fas fa-arrow-alt-circle-down fa-2x"></i></a>
             </div>
             
             
             
         </div>
         
+        <div class="bannerHeading" style="margin-bottom:30px"><p>An influencer agency run by <span>actual</span> influencers</p></div>
+        
+        <span class="anchor" id="sec"></span>
         <div id="section1">
-            <div class="block">
-                
-                <div class="division1">
-                
-                    <p>AN<br />AG<br />EN<br />CY</p>
+            <h2>What we offer</h2>
+            <div id="offerList">
+                <ul>
+                    <a><li><div class="imageDiv"><img src="listitem1.png" /></div><div class="headingDiv"><h3>Talent sourcing</h3></div></li></a>
+                    <a><li><div class="imageDiv"><img src="listitem2.png" /></div><div class="headingDiv"><h3>Brief development</h3></div></li></a>
+                    <a><li><div class="imageDiv"><img src="listitem4.png" /></div><div class="headingDiv"><h3>campaign creation</h3></div></li></a>
+                    <a><li><div class="imageDiv"><img src="listitem3.png" /></div><div class="headingDiv"><h3>contract handling</h3></div></li></a>
+                    <a><li><div class="imageDiv"><img src="listitem5.png" /></div><div class="headingDiv"><h3>content oversight</h3></div></li></a>
+                    <a><li><div class="imageDiv"><img src="listitem6.png" /></div><div class="headingDiv"><h3>talent management</h3></div></li></a>
                     
-                    <img class="divisionImg" src="webdiv1.png" />
-                
-                </div>
-                
-                <div class ="division2">
-                
-                    <p>DE<br />SI<br />GN<br />ED</p>
-                    
-                     <img class="divisionImg" src="webdiv2.jpg" />
-                </div>
-            
-            </div>
-            
-            <div class="block">
-                
-                <div class="division1">
-                
-                    <p>WI<br />TH<br />Y<br />OU</p>
-                
-                     <img class="divisionImg" src="webdiv3.jpg" />
-                </div>
-                
-                <div class ="division2">
-                
-                    <p>IN<br />MI<br />N<br />D</p>
-                    
-                     <img class="divisionImg" src="webdiv4.jpg" />
-                </div>
-            
+                </ul>
             </div>
 
         </div>
         
         
         
-        <div id="section2">
+       <!-- <div id="section2">
         
             <div class="content1 revealRight"><p>INFLUENCER & CELEBRITY MANAGEMENT HANDLED BY THOSE WHO<br /><span> GET IT</span></p></div>
             
@@ -106,91 +133,133 @@ But is every agency run by actual current successful influencers and marketing p
 At Run By Influencers our team has over 8 years experience in their craft and through their own struggles in the industry have created an agency that truly puts the creator and brand first.</p>
             </div>
         
-        </div>
+        </div> -->
         
+        <span class="anchor" id="brandsec"></span>
         <div id="section3">
             
-            <div class="content1 revealOpacity">
-                <h1>We're trusted by...</h1>
-                <ul>
-                    <li><img src="brand1e.png"></li>
-                    <li><img src="brand2e.png"></li>
-                    <li><img src="brand7c.png"></li>
+            <div class="content1">
+                <h1>Our brands</h1>
+                <ul >
                     <li><img src="brand6e.png"></li>
-                    <li><img src="brand3e.png"></li>
+                    <li><img src="brand5f.png"></li>
                     <li><img src="brand4e.png"></li>
-                    <li><img src="brand5e.png"></li>
-                    <li><img src="brand8c.png"></li>
+                    <li><img src="brand3e.png"></li>
+                    <li><img src="brand2e.png"></li>
+                    <li><img src="brand7f.png"></li>
+                    <li><img src="brand1e.png"></li>
+                    <li><img src="brand2f.png"></li>
+                    <li><img src="brand3f.png"></li>
                     <li><img src="brand9e.png"></li>
                     <li><img src="brand10e.png"></li>
+                    <li><img src="brand6f.png"></li>
                 </ul>
             
             </div>
             
-            <div class="content2 revealLeft">
-                <p>“CONTENT IS FIRE; SOCIAL MEDIA IS
-                    GASOLINE.” <br />- <span>JAY BAER</span></p>
+            <div class="content2">
+                <p><span style="font-size:24px;">Successful campaigns = <br /><span style="color:orange;">happy clients</span></span><br /> <br /><br /> <br /><br /> 
+Here at run by influencers, WE
+USE OUR IN HOUSE TALENT ROSTER COMBINED WITH OUR EXTERNAL
+HEADHUNTING SERVICES TO MATCH THE RIGHT INFLUENCER WITH THE
+RIGHT BRAND. <br /><br /><br />
+                    from brief development to content oversight, we're with you every step of the way.</p>
             </div>
+            
+           
         
         
         </div>
         <div id="section4">
-        <div class="content1"><p>OUR <span>PLAN </span>   FOR BRANDS</p></div>
+            
             
             <div class="content2">
-                <p class="revealOpacity"><span>WE SOURCE</span><br /> 
-Whether that's from our
-200+ roster or from our external
-headhunting service, we'll find the perfect match
-                    <br /><br />
-<span>WE MANAGE</span><br /> 
-We handle all connections, including any help with contracts, briefs and invoicing.
-                    <br /><br />
-<span>WE CREATE</span><br /> 
-We know what drives clicks, we we work closely with our talent & you to ensure you get the best R.O.I</p>
+                <span id="plus">+</span>
+                <h2>Our <span>IIP</span> formula</h2>
+                <div class="upperContent"><div class="upperContentList1"><img src="section2listitem1.png" /><p><span>Active Influencer</span> <br /> (someone who has uploaded in the last 28 days)</p></div><div class="upperContentList2"><img src="section2listitem2.png" /><p><span>Technical Data</span> <br /> (analyse their cpm & previous sponsorships)</p></div></div>
+                <div class="lowerContent">
+                    <div class="lowerContentList1"><span id="equals">=</span><img src="section2listitem3.png" /><p><span>Create our magic plan</span> <br /> ((WE PROVIDE
+AN IIP BASED ON YOUR CAMPAIGN GOALS & BUDGET)</p></div>
+                </div>
             </div>
+            
+            <div class="content1"><p><span>metrics that matter <br /> = <span style="color:orange;">A winning creation</span></span><br /> <br /><br /> 
+USING OUR KPI REVIEW METHOD,<br />
+WE CREATE OUR IDEAL INFLUENCER PROFILE- THE IIP APPROACH.
+<br /><br />THIS RESULTS IN THE BEST PERFORMING CAMPAIGNS ENSURING A
+GOOD ROI EVERYTIME.</p></div>
         
         
         </div>
+        
+        <div class="bannerHeading" style="margin-bottom:80px;"><p>Let's create your <span>dream</span> campaign</p></div>
+        
+         <span class="anchor" id="talentsec"></span>
         <div id="section5">
-            <div class="content1"><div id="sideText"><p>SOME OF THE TEAM DOING WHAT THEY DO BEST..</p></div>
+           
+           
+            <div class="content2">
+                <p><span style="font-size:24px;">Top talent = <br /><span style="color:orange;">click-worthy content</span></span><br /> <br /><br /> <br /><br /> 
+ESTABLISHING MEANINGFUL CONNECTIONS WITH OUR TALENT
+ENABLES US TO ADVISE ON CONTENT CREATION AND ACCELERATE
+CHANNEL GROWTH ACROSS YOUTUBE & INSTAGRAM<br /><br /><br />
+                    Unlike other agencies who have faceless databases, we hand pick the very best who are actively creating clickable content.</p>
+            </div>
+            
+             <div class="content1">
             
             <div id="sideVideos">
+                <div id="sideText"><p>Our Campaigns</p></div>
                 
                 <ul>
-                    <li><a href="#"><div class='embed-container'><iframe src='https://www.youtube.com/embed/CS3dpn6owCw' frameborder='0' allowfullscreen></iframe></div></a></li>
-                    <li><a href="#"><div class='embed-container'><iframe src='https://www.youtube.com/embed/fEOHCV-lOjc' frameborder='0' allowfullscreen></iframe></div></a></li>
-                    <li><a href="#"><div class='embed-container'><iframe src='https://www.youtube.com/embed/4aYTa7bQA34' frameborder='0' allowfullscreen></iframe></div></a></li>
+                    <li><a href="#"><div class='embed-container'><iframe src='https://www.youtube.com/embed/jq9wpv0tM1Q' frameborder='0' allowfullscreen></iframe></div></a></li>
+                    <li><a href="#"><div class='embed-container'><iframe src='https://www.youtube.com/embed/O-bM1Bru79s' frameborder='0' allowfullscreen></iframe></div></a></li>
+                    <li><a href="#"><div class='embed-container'><iframe src='https://www.youtube.com/embed/u_hZC0Yl5Ow' frameborder='0' allowfullscreen></iframe></div></a></li><li><a href="#"><div class='embed-container'><iframe src='https://www.youtube.com/embed/5oaYPcojNOg' frameborder='0' allowfullscreen></iframe></div></a></li><li><a href="#"><div class='embed-container'><iframe src='https://www.youtube.com/embed/GqbNFCMkEYY' frameborder='0' allowfullscreen></iframe></div></a></li>
                 </div>
             
             </div>
-            <div class="content2 revealLeft"><p>YOU <span>CAN</span> SIT WITH US.</p></div>
-            
-            <div class="content3 revealLeft">
-                <p>Whether you're an influencer, celeb, baker, cook, dog walker, hairdresser, beauty guru, fitness freak or just damn amazing - if you're creating engaging content, we'd love to work with you.</p>
-            </div>
         
         </div>
-        <div id="section6">
-            
-
-            <div class="content1"><p>HELLO, CAN WE <span>HELP </span>   YOU?</p></div>
+            <div id="section4b">
+                <div class="content1"><p><span>incredible content <br /> = <span style="color:orange;">deserves to be paid content</span></span><br /> <br /><br /> 
+We value your time and creativity,<br /> so we only seek out paid opportunities.<br />we handle all your contracts and payouts<br /> so you can focus on what you do best.</p></div>
             
             <div class="content2">
-                <i class="fas fa-paperclip fa-5x"></i>
+                
+                <h2>How we work with <span>talent</span></h2>
+                <div class="upperContent"><div class="upperContentList1"><img src="secondIcon4.png" /><p><span >Exclusive or non-exclusive</span> <br /> (we offer flexible choices to suit you, zero pressure)</p></div><div class="upperContentList2"><img src="secondIcon1.png" /><p><span >Source opportunities</span> <br /> (we actively seek out new partnership possibilities)</p></div></div>
+                <div class="lowerContent">
+                    <div class="lowerContentList1"><img src="secondIcon2.png" /><p><span >give you the choice</span> <br /> (you are in full control of what campaigns you accept)</p></div><div class="lowerContentList2"><img src="secondIcon3.png" /><p><span >handle the paperwork</span> <br /> (we take care of briefs, contracts, payments - so you can just create)</p></div>
+                </div>
+            </div>
+        
+        
+        </div>
+            
+            <div class="bannerHeading" style="margin-bottom:40px;hei"><p>start earning more for what you do <span>best</span> </p></div>
+       
+            
+             <span class="anchor1" id="contactsec"></span>
+            <div id="section6">
+            
+
+                <div class="content1"><p>Contacting us = <br /> <span>the best thing you'll ever do</span></p></div>
+            
+            <div class="content2">
+               
                 <p><br /> 
-We're here to make your life as a brand, fellow agency or influencer simpler. Get in touch and see how we can help.
+Get in touch and see how we can help.
                     <br /><br />
                  <!--alert messages start-->
     <?php echo $alert; ?>
     <!--alert messages end-->
 <form method="post" name="contactform" action="#myform" id="myform">
-    <input type="text" id="name" name="name" placeholder="Your name..">
+    <input type="text" id="name" name="name" placeholder="Your name">
 
-    <input type="email" id="email" name="email" placeholder="Your Email.."required>
+    <input type="email" id="email" name="email" placeholder="Your Email"required>
     
-    
-    <input type="text" id="message" name="message" placeholder="Message..." required>
+
+    <textarea  id="message" name="message" placeholder="Enter Message" required></textarea>
 
   
     <input type="submit" name="submit" value="SEND">
@@ -207,20 +276,62 @@ We're here to make your life as a brand, fellow agency or influencer simpler. Ge
         </div>
         
         <footer>
+            <div id="footerLeft">
+                <p>INTERNATIONAL HOUSE,
+24 HOLBORN VIADUCT,
+LONDON, UNITED KINGDOM, EC1A 2BN</p>
+                <img src="logofooter.png" />
+                
+                
+                <ul>
+                    <li>General: <a href="mailto:hello@runbyinfluencers.com">hello@runbyinfluencers.com</a></li>
+                    <li>Jobs: <a href="mailto:join@runbyinfluencers.com">join@runbyinfluencers.com</a></li>
+                    <li>Press: <a href="mailto:press@runbyinfluencers.com">press@runbyinfluencers.com</a></li>
+                    <li>+44 7375 545626</li>
+                    
+                    <br />
+            <br />
+            
+            <li>Privacy Policy</li>
+            <li>terms & conditions</li>
+                </ul>
+            </div>
+            <div id="footerRight">
+                
+                <h2>Follow us</h2>
+                <ul>
+                    <li><a href="https://www.twitter.com/runbyinfluence"><i class="fab fa-twitter fa-lg" ></i></a>
+
+</li>
+                    <li><a href="https://www.instagram.com/runbyinfluencers/"><i class="fab fa-instagram fa-lg"></i></a>
+
+</li>
+                    <li><a href="https://www.facebook.com/Run-by-Influencers-111447903944476"><i class="fab fa-facebook-square fa-lg"></i></a>
+
+</li>
+                    <li><a href="https://www.youtube.com/channel/UCLJXlnxAJz0PTPicGiK3omw?"><i class="fab fa-youtube fa-lg"></i></a>
+
+</li>
+                    
+            
+            
+                </ul>
+            
+            </div>
+        
+        
+        <div id="copyright"><p>copyright &#169; reserved for runbyinfluencers</p></div>
+            
+            <!--
             <p id ="leftFoot">NTERNATIONAL HOUSE,<br />
                 24 HOLBORN VIADUCT, <br />LONDON, UNITED KINGDOM, EC1A 2BN<br /><a href="#">hello@runbyinfluencers.com</a></p>
-            <p id ="middleFoot">Run By Influencers is a trading name of RBI Media Limited.<br /> Company Number: 12813700</p>
+            <p id ="middleFoot"></p>
             <p id ="rightFoot"><a href="#">Privacy policy</a><br /><a href="#">Terms & conditions</a><br /><a href="#">Vacancies</a></p>
+-->
         
         
         </footer>
         
-        
-        
-        <script> function scrollWin(){
-            var x = document.body.scrollHeight;
-            var y = x-900;
-            window.scrollTo({ top: y, behavior: 'smooth' })}</script>
         
              <script type="text/javascript">
     window.addEventListener('scroll', reveal);
@@ -286,6 +397,7 @@ We're here to make your life as a brand, fellow agency or influencer simpler. Ge
         }
       }
     }
+           
     </script>
             
             
